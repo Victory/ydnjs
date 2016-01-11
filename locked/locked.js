5a60fe4141d0e7bfe985071772564a865b26d618
Victory <git@dfhu.org>
2016-01-11 17:57:08 -0500

    locked

---
+++ locked/locked.js
@@ -0,0 +1,43 @@
+"use strict";
+log('running protected.js');
+
+function Ping (name) {
+    var numPings = 0;
+    var lockVar = 0;
+    var lockKey = 100;
+
+    var myName = null;
+    var P = {
+        ping: function () {
+            numPings += 1;
+            log('ping num ' + numPings + " for " + myName + " : " + lockVar);
+        },
+        incLock: function () {
+            lockKey += 1;
+            if ((lockKey - 100) == 1) {
+                lockVar += 1;
+            }
+        },
+        init: function (n) {
+            myName = n;
+        },
+    };
+
+    P.init(name);
+
+    return P;
+}
+
+var ping1 = new Ping('ping1');
+var ping2 = new Ping('ping2');
+ping1.ping();
+ping1.incLock();
+ping1.ping();
+ping2.ping();
+ping2.ping();
+try {
+    ping1.P.init('foo');
+} catch (e) {
+    log(e);
+}
+ping1.ping();
